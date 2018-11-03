f83a7ca5a699cee434d900efbbab44908736664a
Driver <peplinskikrzysztof@gmail.com>
2018-11-03 14:03:11 +0100

    creating data model and repositories

---
+++ src/main/java/com/spring5webfluxrest/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.spring5webfluxrest.repositories;
+
+import com.spring5webfluxrest.domain.Category;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface CategoryRepository extends ReactiveMongoRepository<Category,String> {
+}
