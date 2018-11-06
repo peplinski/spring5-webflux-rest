d9f3191c598a0eeba6a3befb24713f66ea3249fb
Driver <peplinskikrzysztof@gmail.com>
2018-11-06 17:53:32 +0100

    adding vendors create and update test

--- src/test/java/com/spring5webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/com/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -6,11 +6,13 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 
 public class VendorControllerTest {
 
@@ -48,5 +50,34 @@ public class VendorControllerTest {
                 .uri("/vendors/someID")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    public void  testCreateVendor(){
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().firstName("Chuck").firstName("Norris").build()));
+
+        Mono<Vendor> manSaveMono= Mono.just(Vendor.builder().firstName("fName").lastName("lName").build());
+
+        webTestClient.post()
+                .uri("/vendors")
+                .body(manSaveMono,Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
+    }
+
+    @Test
+    public void testUpdateVendor(){
+        BDDMockito.given(vendorRepository.save(any(Vendor.class)))
+                .willReturn(Mono.just(Vendor.builder().build()));
+
+        Mono<Vendor> manSaveMono= Mono.just(Vendor.builder().firstName("fName").lastName("lName").build());
+
+        webTestClient.put()
+                .uri("vendors/someid")
+                .exchange()
+                .expectStatus()
+                .isOk();
     }
 }
